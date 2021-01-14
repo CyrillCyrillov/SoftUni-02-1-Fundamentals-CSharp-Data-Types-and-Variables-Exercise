7189f07d86c335dad88f9d142ef02cf4350a730a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:27:06 +0200

    Add files via upload

---
+++ Task02_Sum_Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task02_Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (true)
+            {
+                sum += number % 10;
+                number = number / 10;
+                if (number == 0) break;
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
