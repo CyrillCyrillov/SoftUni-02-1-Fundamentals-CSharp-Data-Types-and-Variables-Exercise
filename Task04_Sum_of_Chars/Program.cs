dfda8cce6b89999fb5a50de5f0357c929b5bd91d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:28:46 +0200

    Add files via upload

---
+++ Task04_Sum_of_Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task04_Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                string next = Console.ReadLine();
+                char letter = next[0];
+                sum += letter;
+            }
+            
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
