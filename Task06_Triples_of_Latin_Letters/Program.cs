8476147333c4e4d969e978412bc1285bc35f4a10
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:30:20 +0200

    Add files via upload

---
+++ Task06_Triples_of_Latin_Letters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task06_Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (int k = 1; k <= n; k++)
+                    {
+                        Console.Write((char)(96 + i));
+                        Console.Write((char)(96 + j));
+                        Console.Write((char)(96 + k));
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
