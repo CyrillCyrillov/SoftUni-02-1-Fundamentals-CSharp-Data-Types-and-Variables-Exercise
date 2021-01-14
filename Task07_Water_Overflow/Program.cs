c38d7062fdcdbfb1e2b3c351a2c1bf1828a34093
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:31:09 +0200

    Add files via upload

---
+++ Task07_Water_Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task07_Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short capacity = 255;
+            short n = short.Parse(Console.ReadLine());
+            short litersInTheTank = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                short liters = short.Parse(Console.ReadLine());
+                if (liters + litersInTheTank > capacity)
+                    {
+                        Console.WriteLine("Insufficient capacity!");
+                    }
+                else
+                    {
+                        litersInTheTank += liters;
+                    }
+            }
+            
+            Console.WriteLine(litersInTheTank);
+        }
+    }
+}
