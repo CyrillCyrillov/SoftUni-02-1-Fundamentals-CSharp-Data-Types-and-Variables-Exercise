1b977d26a79861ee8a9930f003934016c0d69b86
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:27:57 +0200

    Add files via upload

---
+++ Task03_Elevator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task03_Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            
+            if (persons <= capacity)
+                {
+                    Console.WriteLine(1);
+                }
+            else if (persons % capacity == 0 )
+                {
+                    Console.WriteLine(persons / capacity);
+                }
+            else
+                {
+                    Console.WriteLine(persons / capacity + 1);
+                }
+        }
+    }
+}
