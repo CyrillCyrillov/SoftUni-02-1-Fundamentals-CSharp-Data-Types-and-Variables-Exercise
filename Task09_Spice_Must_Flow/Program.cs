eee9a7c740a221ca5777a24207ec7ee6cf49c0e5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:32:51 +0200

    Add files via upload

---
+++ Task09_Spice_Must_Flow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task09_Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayYield = int.Parse(Console.ReadLine());
+            int daysWork = 0;
+            int totalAmount = 0;
+
+            if (dayYield >= 100)
+            {
+                while (dayYield >= 100)
+                {
+                    totalAmount += dayYield;
+                    totalAmount -= 26;
+                    daysWork += 1;
+                    dayYield -= 10;
+                }
+
+                totalAmount -= 26;
+            }
+            Console.WriteLine(daysWork);
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
