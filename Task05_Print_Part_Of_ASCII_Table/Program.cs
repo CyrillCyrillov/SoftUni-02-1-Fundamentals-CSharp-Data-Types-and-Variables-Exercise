86ab6388b63ba557f5ecdf999d9663c83b99dbb8
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:29:34 +0200

    Add files via upload

---
+++ Task05_Print_Part_Of_ASCII_Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task05_Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int begin = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = begin; i <= end; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
