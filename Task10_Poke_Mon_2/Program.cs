264fa70ffea9505aa30893f7588943ee1a9cd792
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:34:32 +0200

    Add files via upload

---
+++ Task10_Poke_Mon_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task10_Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int targets = 0;
+            double nStartingValue = n;
+
+            while (n >= m)
+            {
+                n -= m;
+                targets++;
+                
+                if (n == Math.Floor(nStartingValue / 2) && y != 0)
+                {
+                    n /= y;
+                }
+                
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(targets);
+        }
+    }
+}
